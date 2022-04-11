3d58df43def1e9e4ab1dacbac2137183dc81e945
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-11 19:15:04 +0200

    Copy license header

---
+++ GPL_2_0.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * \author      Kevin Matthes
+ * \brief       The brief license text of the GPL 2.0 license.
+ * \copyright   (C) 2022 Kevin Matthes.
+ *              This file is licensed GPL 2 as of June 1991.
+ * \date        2022
+ * \file        GPL_2_0.h
+ * \note        See `LICENSE' for full license.
+ *              See `README.md' for project details.
+ *
+ * This header defines the brief license text of the GPL 2.0 license as a macro.
+ */
+
+/******************************************************************************/
+
+/*
+ * Security settings.
+ */
+
+#pragma once
+#ifndef __GPL_2_0_H__
+#define __GPL_2_0_H__
+
+
+
+/**
+ * \brief   The GPL 2.0 license (brief version).
+ *
+ * This macro represents the brief version of the GPL 2.0 license text.
+ */
+
+#define GPL_2_0 \
+    "This program is free software; you can redistribute it and/or modify\n"   \
+    "it under the terms of the GNU General Public License as published by\n"   \
+    "the Free Software Foundation; either version 2 of the License, or\n"      \
+    "(at your option) any later version.\n"                                    \
+    "\n"                                                                       \
+    "This program is distributed in the hope that it will be useful,\n"        \
+    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"         \
+    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"          \
+    "GNU General Public License for more details.\n"                           \
+    "\n"                                                                       \
+    "You should have received a copy of the GNU General Public License along\n"\
+    "with this program; if not, write to the Free Software Foundation, Inc.,\n"\
+    "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA."
+
+
+
+/*
+ * End of header.
+ */
+
+// Leaving the header.
+#endif  // ! __GPL_2_0_H__
+
+/******************************************************************************/
