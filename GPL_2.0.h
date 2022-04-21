18c9c13f8c7be716781ab9c7e60e9892886e1771
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-21 21:26:36 +0200

    Save warranty section

--- GPL_2.0.h
+++ GPL_2.0.h
@@ -47,7 +47,7 @@
  * This macro represents the brief version of the GPL 2.0 license text.
  */
 
-#define GPL_2_0 \
+#define GPL_2_0                                                                \
     "This program is free software; you can redistribute it and/or modify\n"   \
     "it under the terms of the GNU General Public License as published by\n"   \
     "the Free Software Foundation; either version 2 of the License, or\n"      \
@@ -70,7 +70,28 @@
  * This macro represents the entire warranty section of the GPL 2.0 license.
  */
 
-#define GPL_2_0__WARRANTY ""
+#define GPL_2_0__WARRANTY                                                       \
+"                            NO WARRANTY\n"                                     \
+"\n"                                                                            \
+"  11. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY\n"  \
+"FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN\n"    \
+"OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES\n"      \
+"PROVIDE THE PROGRAM \"AS IS\" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED\n"\
+"OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF\n"        \
+"MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS\n"   \
+"TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE\n"      \
+"PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING,\n"    \
+"REPAIR OR CORRECTION.\n"                                                       \
+"\n"                                                                            \
+"  12. IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING\n" \
+"WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR\n"         \
+"REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES,\n"  \
+"INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING\n" \
+"OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED\n"   \
+"TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY\n"    \
+"YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER\n"  \
+"PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE\n"       \
+"POSSIBILITY OF SUCH DAMAGES."
 
 
 
