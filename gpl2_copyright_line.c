e49b408fe034286f9e6142c162e2e5d87f6d16ca
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2022-04-23 20:55:10 +0200

    Bugfix

--- gpl2_copyright_line.c
+++ gpl2_copyright_line.c
@@ -22,7 +22,7 @@
  * \copyright   (C) 2022 Kevin Matthes.
  *              This file is licensed GPL 2 as of June 1991.
  * \date        2022
- * \file        gpl2_license_line.c
+ * \file        gpl2_copyright_line.c
  * \note        See `LICENSE' for full license.
  *              See `README.md' for project details.
  *
